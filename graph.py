153ceb02ed4b23231d68802f8f21be1a112bbded
bryan kennedy <bkennedy@smm.org>
2013-12-06 12:35:05 -0600

    Adding a script to graph the energy data
    This is a prototype and isn't currently working. Need to make it more
    flexible and figure out why the CSV isn't parsing correctly.

---
+++ graph.py
@@ -0,0 +1,44 @@
+"""Graph CSV energy data
+
+Usage:
+    graph.py <input_file>
+    graph.py (-h | --help)
+    graph.py --version
+
+Options:
+    -h --help    Show this help.
+    --version    Show version number.
+
+"""
+from docopt import docopt
+import csv
+import matplotlib
+import pylab
+
+
+def getColumn(filename, column):
+    o = open(filename, 'rU')
+    results = csv.reader(o)
+    print results
+    #results = csv.reader(open("data_2004-2013.csv"), delimiter="\t")
+    return [result[column] for result in results]
+
+
+def make_graph(input_file):
+    reader = csv.reader(open(input_file))
+    date = []
+    buy = []
+
+    for _ in xrange(3):
+        next(reader)
+
+    for line in reader:
+        date.append(line[4])
+        buy.append(line[5])
+
+    matplotlib.pyplot.scatter(date, buy)
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__, version='Graph 0.1')
+    make_graph(args['<input_file>'])
